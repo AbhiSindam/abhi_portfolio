030cbd709b3ee50386a124a7bb9c19c8ff429be9
AbhiSindam <abhisindam206@gmail.com>
2022-06-08 09:56:01 +0530

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => (
       <Link href='/'>
         <a style={{ display: "flex", alignItems: "center", color: "white" }}>
           {/* <DiCssdeck size = "3rem" />  */}
-          <Span>{`< Abhishek />`}</Span>
+          <Span>{`< Abhi />`}</Span>
         </a>
       </Link>
     </Div1>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -19,6 +19,16 @@ export const projects = [
     visit: "https://hilarious-baklava-9743c9.netlify.app/",
     id: 1,
   },
+  {
+    title: "Fraazo",
+    description:
+      "In this project we have created user interface of groww webpage and added backend. Here user is able to see all the stocks and buy them, sell and delete stocks from cart.",
+    image: "/images/3.png",
+    tags: ["HTML","CSS","React","Redux"],
+    source: "https://github.com/amankatiyar321/Farrroz-fro",
+    visit: "https://extraordinary-blancmange-42c3dc.netlify.app/",
+    id: 2,
+  },
 ];
 
 // git commit
